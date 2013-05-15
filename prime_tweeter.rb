0bc402f3cf434f7a4700b3598880b85eb9d622ed
Terry G Lorber 2 <tgl@rideside.net>
2013-05-14 20:42:36 -0400

    COMP bad syntax

--- prime_tweeter.rb
+++ prime_tweeter.rb
@@ -5,7 +5,7 @@ require 'twitter'
 class PrimeTweeter
 
   def self.post
-    @twitter ||= Twitter.Client.new(
+    @twitter ||= Twitter::Client.new(
       consumer_key: ENV['TWITTER_CONSUMER_KEY'],
       consumer_secret: ENV['TWITTER_CONSUMER_SECRET'],
       oauth_token: ENV['TWITTER_OAUTH_TOKEN'],
