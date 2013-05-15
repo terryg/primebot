aaf77d8d40778e62d7206481415abc0e96639634
Terry G Lorber 2 <tgl@rideside.net>
2013-05-14 20:23:16 -0400

    ENH make this a class method

--- prime_tweeter.rb
+++ prime_tweeter.rb
@@ -4,7 +4,7 @@ require 'twitter'
 
 class PrimeTweeter
 
-  def post
+  def self.post
     @twitter ||= Twitter.Client.new(
       consumer_key: ENV['TWITTER_CONSUMER_KEY'],
       consumer_secret: ENV['TWITTER_CONSUMER_SECRET'],
